f6066b7c942a9605478704dd53a2e4ea1993ddaf
andLyth <43781138+andLyth@users.noreply.github.com>
2021-08-16 19:49:20 +0200

    reading file
    Messy, but it reads file and pushes to queue

--- getTemperature/main.cpp
+++ getTemperature/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <fstream>          //reading file with ADC values
+#include <queue>            //std::queue
+#include <iomanip>          //std::setprecision
 
-using namespace std;
+
+std::string readFile(void);
+
+std::queue<double> temperQueue;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double test = 0.0;
+    while(true)
+    {
+        test = temperQueue.front();
+        temperQueue.pop();
+        std::cout << std::setprecision(2) << std::fixed;
+        std::cout << test << std::endl;
+    }
+
     return 0;
+}
+
+
+std::string readFile(void)
+{
+    std::string str;
+
+    try
+    {
+        std::ifstream temperStream("temperature.txt");
+        while (std::getline(temperStream, str))
+        {
+            temperQueue.push (stod(str));
+        }
+    }
+    catch (const std::ifstream::failure& e)
+    {
+        std::cout << "Exception opening/reading file" << std::endl;
+    }
+
+
+
+
+
 }
