ef1ba20dd0bee69c28eec5f9a174659127bb4a47
andLyth <43781138+andLyth@users.noreply.github.com>
2021-08-16 21:11:10 +0200

    added periodic function call for printing data
    added periodic func call for testing reading data from the queue.

--- getTemperature/main.cpp
+++ getTemperature/main.cpp
@@ -8,7 +8,9 @@
 #include <thread>           //for multithreading
 
 
-#define WAIT_PERIOD_FETCH 10
+
+#define WAIT_PERIOD_FETCH 2000
+#define WAIT_PERIOD_SEND 2000
 
 template <class Precision>
 std::string getISOCurrentTimestamp()
@@ -18,6 +20,7 @@ std::string getISOCurrentTimestamp()
 }
 
 void readFile(void);
+void printData(void);
 void timer_start(std::function<void(void)> func);
 
 std::queue<double> temperQueue;
@@ -27,15 +30,14 @@ int main()
 {
     /*starting periodic fuction call to read file with ADC values*/
     timer_start(readFile);
+    timer_start(printData);
 
 
     /*test loop for checking file reading*/
     while(true)
     {
 
-        std::cout << std::setprecision(2) << std::fixed;
-        std::cout << temperQueue.front() << std::endl;
-        temperQueue.pop();
+
     }
 
     return 0;
@@ -55,13 +57,26 @@ void readFile(void)
     temperStream.close();
     std::this_thread::sleep_until(wakeUpTime);
 
-
-
 }
 
-void timer_start(std::function<void(void)> func){
+void printData(void)
+{
+    auto wakeUpTime = std::chrono::steady_clock::now() + std::chrono::milliseconds(WAIT_PERIOD_SEND);
+    while (!temperQueue.empty())
+    {
+        std::cout << std::setprecision(2) << std::fixed;
+        std::cout << temperQueue.front() << std::endl;
+        temperQueue.pop();
+    }
+    std::this_thread::sleep_until(wakeUpTime);
+}
+
+
+void timer_start(std::function<void(void)> func)
+{
   std::thread([func](){
-    while (true){
+    while (true)
+    {
       func();
     }
   }).detach();
