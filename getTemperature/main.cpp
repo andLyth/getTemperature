94eb31904247a04bb687b2f95e385b7d94897b8c
andLyth <43781138+andLyth@users.noreply.github.com>
2021-08-16 20:21:40 +0200

    Periodic function call
    Added periodic function call.

--- getTemperature/main.cpp
+++ getTemperature/main.cpp
@@ -2,21 +2,40 @@
 #include <fstream>          //reading file with ADC values
 #include <queue>            //std::queue
 #include <iomanip>          //std::setprecision
+#include <chrono>           //std::chrono
+#include <date.h>           //for time format (UTC)
+#include <functional>       //std::function <-- thread/timer
+#include <thread>           //for multithreading
 
+
+#define WAIT_PERIOD_FETCH 10
+
+template <class Precision>
+std::string getISOCurrentTimestamp()
+{
+    auto now = std::chrono::system_clock::now();
+    return date::format("%FT%TZ", date::floor<Precision>(now));
+}
 
 std::string readFile(void);
+void timer_start(std::function<void(void)> func);
 
 std::queue<double> temperQueue;
+std::string nowTime = getISOCurrentTimestamp<std::chrono::microseconds>();
 
 int main()
 {
-    double test = 0.0;
+    /*starting periodic fuction call to read file with ADC values*/
+    timer_start(readFile);
+
+
+    /*test loop for checking file reading*/
     while(true)
     {
-        test = temperQueue.front();
-        temperQueue.pop();
+
         std::cout << std::setprecision(2) << std::fixed;
-        std::cout << test << std::endl;
+        std::cout << temperQueue.front() << std::endl;
+        temperQueue.pop();
     }
 
     return 0;
@@ -26,22 +45,24 @@ int main()
 std::string readFile(void)
 {
     std::string str;
+    std::ifstream temperStream("temperature.txt");
+    auto wakeUpTime = std::chrono::steady_clock::now() + std::chrono::milliseconds(WAIT_PERIOD_FETCH);
 
-    try
+    while (std::getline(temperStream, str))
     {
-        std::ifstream temperStream("temperature.txt");
-        while (std::getline(temperStream, str))
-        {
-            temperQueue.push (stod(str));
-        }
+        temperQueue.push(stod(str));
     }
-    catch (const std::ifstream::failure& e)
-    {
-        std::cout << "Exception opening/reading file" << std::endl;
+    temperStream.close();
+    std::this_thread::sleep_until(wakeUpTime);
+
+
+
+}
+
+void timer_start(std::function<void(void)> func){
+  std::thread([func](){
+    while (true){
+      func();
     }
-
-
-
-
-
+  }).detach();
 }
