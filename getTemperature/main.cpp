18033fc812930972481717402c7b2a5ef6c7bc5c
andLyth <43781138+andLyth@users.noreply.github.com>
2021-08-18 02:43:42 +0200

    Convert ADC values to temperature
    Conversion to temperature, removing testcode.

--- getTemperature/main.cpp
+++ getTemperature/main.cpp
@@ -7,11 +7,14 @@
 
 #define WAIT_PERIOD_FETCH 2000
 #define WAIT_PERIOD_SEND 2000
+#define SLOPE 23.0/952.0d
+#define INTERCEPT 50.0d
 
 
 void readFile(void);
 void printData(void);
 void timer_start(std::function<void(void)> func);
+double convertToTemp(double adcValue);
 
 std::queue<double> temperQueue;
 std::string nowTime = getISOCurrentTimestamp<std::chrono::microseconds>();
@@ -46,23 +49,18 @@ void readFile(void)
     {
         while (std::getline(temperStream, str))
         {
-            temperQueue.push(stod(str));
+            temperQueue.push(convertToTemp(stod(str)));
         }
-
+        temperStream.close();
         std::this_thread::sleep_until(wakeUpTime);
-                temperStream.close();
-    std::cout << "stream closed" << std::endl;
-    std::cout << "stream closed" << std::endl;
-    std::cout << "stream closed" << std::endl;
-    std::cout << "stream closed" << std::endl;
-    std::cout << "stream closed" << std::endl;
     }
-    else {
-        // show message:
+    else
+    {
         std::cout << "Error opening file";
     }
 }
 
+/*function for code testing*/
 void printData(void)
 {
     auto wakeUpTime = std::chrono::steady_clock::now() + std::chrono::milliseconds(WAIT_PERIOD_SEND);
@@ -71,8 +69,16 @@ void printData(void)
         std::cout << std::setprecision(2) << std::fixed;
         std::cout << temperQueue.front() << std::endl;
         temperQueue.pop();
-    }
+
     std::this_thread::sleep_until(wakeUpTime);
+    }
+}
+
+
+/*linear function for adc -> temp conversion: f(x) = (23/952)*x - 50 */
+double convertToTemp(double adcValue)
+{
+    return SLOPE * adcValue - INTERCEPT;
 }
 
 
